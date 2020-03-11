3cacdc2629d772540a739f4dcca80130f916d0cf
uinstinct <uinstinct@outlook.com>
2020-03-12 01:34:10 +0530

    working towards authentication

--- sociostudents/views.py
+++ sociostudents/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Student
 from .forms import StudentForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def homepage(request):
@@ -11,6 +12,7 @@ def all_students(request):
     students = Student.objects.all()
     return render(request, 'students/all_students.html', {'students': students})
 
+@login_required
 def list_student(request, username):
     student =  get_object_or_404(Student, username=username)
     return render(request, 'students/profilepage.html', {'student':student})
