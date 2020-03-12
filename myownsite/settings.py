390b08a893c9ff5f4694b2b2b4916b8e6fefcde8
uinstinct <uinstinct@outlook.com>
2020-03-12 18:17:23 +0530

    reconfigured the navbar

--- myownsite/settings.py
+++ myownsite/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIR= [
+    os.path.join(BASE_DIR,"static"),
+    '/sociostudents/static/'
+]
 
 
 LOGIN_REDIRECT_URL = '/students'
